6bb2432a255d3c7f83f53b163b63a0e5f4c97f1f
Pascal Precht <pascal.precht@googlemail.com>
2012-10-29 16:03:22 +0100

    Built a few constructors to abstract the tabs away

--- jquery.neotabs.js
+++ jquery.neotabs.js
@@ -2,75 +2,76 @@
 
   var pluginName = 'neoTabs',
       document = window.document,
+
+      // Default options
       defaults = {
-        wrapperClass: 'content',
-        activeClass: 'active',
-        tabheadElement: 'h4',
-        tabheadClass: '.tab',
-        tabbody: '.tabbody',
-        fx: 'show',
-        fxSpeed: 'normal',
-        activeInfoText: '',
-        activeInfoPosition: 'prepend',
-        activeInfoClass: 'active-info',
-        tabsListClass: 'tabs-list',
-        syncheights:false,
-        syncHeightMethodName:'syncHeight',
-        cssClassAvailable:false,
-        saveState: false,
-        autoAnchor: false,
-        pagination: false,
-        tabsPostion: 'top',
-        wrapInnerTabs: '',
-        firstTabClass: 'first',
-        lastTabClass: 'last',
-        clearfixClass: 'group'
+        wrapperClass: 'content', // Classname to apply to the div that wraps
+        activeClass: 'active', // Active tab classname
+        tabheadElement: 'h4', // Elements to transform to tabs
+        tabheadClass: '.tab', // Tab classname
+        tabbody: '.tabbody', // Tabbody classname
+        fx: 'show', // Default effect for collapsing content
+        fxSpeed: 'normal', // Effect speed
+        tabsListClass: 'tabs-list', // Classname to apply to generated tabs list
+        cssClassAvailable:false, // Applying original class names to tabs or not
+        autoAnchor: false, // Making tabs linkable
+        tabsPostion: 'top', // Position of tabs 'top' or 'bottom'
+        wrapInnerTabs: '', // InnerWrap for tabs
+        firstTabClass: 'first', // Classname for the first tab
+        lastTabClass: 'last', // Classname for the last tab
+        clearfixClass: 'group', // Name of the class that is used to clear floats
       };
 
-  function uniqueId (p, q, r) {
-    r = (r === undefined) ? '' : '-'+r;
-    return p + q + r;
-  }
+  // Constructor
+  function NeoTabs(element, options) {
+    _this = this;
 
-  function NeoTabs( element, options ) {
-    this.element = element;
-    this.options = $.extend( {}, defaults, options) ;
-    this._defaults = defaults;
-    this._name = pluginName;
-    this.list = '';
-    this.tabCound = 0;
-    this.$el = $(this.element);
-
-
-    this.$el.wrapInner('<div class="' + this.options.wrapperClass + '"></div>');
-
-    this.$el.find(this.options.tabheadElement).each(function (i) {
-      var id = '',
-          elId = $(this).attr('id');
-
-      if (elId) {
-        if (elId.indexOf('accessibletabscontent') === 0) {
-          return;
-        }
-        id = 'id="' + elId + '"';
-      }
-      var tabId = uniqueId('accessibletabscontent',
+    $.extend(_this, {
+      $el: element,
+      options: $.extend({}, defaults, options)
     });
 
+    // This represents our tab list
+    var tabs = new TabList();
 
-    this.init();
-  }
+    _this.$el.find(_this.options.tabheadElement).each(function (i) {
+      // Let's cache this query
+      var $currentEl = $(this);
 
-  NeoTabs.prototype.init = function () {
+      // And create a new tab object from it
+      var tab = new Tab({
+        label: $currentEl.html(),
+        id: $currentEl.attr('id')
+      });
 
+      // Allright, we're done!
+      tabs.addTab(tab);
+    });
+
+  };
+
+  function Tab(options) {
+    this.id = options.id;
+    this.label = options.label;
+
+    if (this.id) {
+
+    }
+  };
+
+  function TabList() {
+    this.tabs = [];
+  };
+
+  TabList.prototype.addTab = function (tab) {
+    this.tabs.push(tab);
   };
 
   $.fn[pluginName] = function ( options ) {
     return this.each(function () {
       if (!$.data(this, 'plugin_' + pluginName)) {
-        $.data(this, 'plugin_' + pluginName, new NeoTabs( this, options ));
+        $.data(this, 'plugin_' + pluginName, new NeoTabs($(this), options ));
       }
     });
   }
-
 }(jQuery, window));
