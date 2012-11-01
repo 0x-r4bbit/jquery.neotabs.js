fd5c4d56509fc4f2b8b257a42e8c4066dcb90a07
Pascal Precht <pascal.precht@googlemail.com>
2012-11-01 11:22:36 +0100

    Updated readme and added jquery-jasmine

--- tests/unit/specs.js
+++ tests/unit/specs.js
@@ -3,26 +3,5 @@
 
 describe('jQuery NeoTabs tests', function () {
 
-  var fixture;
 
-  beforeEach(function () {
-    fixture = $('<div class="tabs">' +
-      '<div class="tabbody">' +
-        '<h4>Heading</h4>' +
-        '<p>Lorem Ipsum</p>' +
-      '</div>' +
-      '<div class="tabbody">' +
-        '<h4>Heading 2</h4>' +
-        '<p>Lorem Ipsum</p>' +
-      '</div>' +
-      '<div class="tabbody">' +
-        '<h4>Heading 3</h4>' +
-        '<p>Lorem Ipsum</p>' +
-      '</div>' +
-    '</div>');
-  });
-
-  it('shoud return true if NeoTabs is available', function () {
-    expect(typeof($.fn.neoTabs)).toBe('function');
-  });
 });
