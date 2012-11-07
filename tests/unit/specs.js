8a0dc4ff9cdb59f0a56c58ffbcc7f43e919cfb8d
Pascal Precht <pascal.precht@googlemail.com>
2012-11-07 14:55:42 +0100

    Moar tests

--- tests/unit/specs.js
+++ tests/unit/specs.js
@@ -3,9 +3,10 @@
 
 describe('jQuery NeoTabs TestSuite', function () {
 
-  var fixture;
+  var $fixture;
 
-  describe('Dependency Suite', function () {
+  describe('Dependency TestSuite', function () {
+
     it('tests if jQuery is loaded', function () {
       expect(!!window.$).toBe(true);
       expect(typeof(window.$)).toBe('function');
@@ -14,12 +15,47 @@ describe('jQuery NeoTabs TestSuite', function () {
     it('tests if NeoTabs is a jQuery plugin', function () {
       expect(typeof($.fn.neoTabs)).toBe('function');
     });
+
+    it('tests if NeoTabs constructor is avaible in global scope', function () {
+      expect(!!window.NeoTabs).toBe(true);
+      expect(typeof(window.NeoTabs)).toBe('function');
+    });
   });
 
-  describe('DOM Manipulation Suite', function () {
 
-    it('tests if a is b', function () {
-      expect(false).toBe(true);
+  describe('DOM Manipulation TestSuite', function () {
+
+    beforeEach(function () {
+      $fixture = $(
+        '<div class="tabs">' +
+          '<div class="tabbody">' +
+            '<h4>Tabbody Heading</h4>' +
+            '<p>Lorem Ipsum</p>' +
+          '</div>' +
+          '<div class="tabbody">' +
+            '<h4>Tabbody Heading 2</h4>' +
+            '<p>Lorem Ipsum</p>' +
+          '</div>' +
+          '<div class="tabbody">' +
+            '<h4>Tabbody Heading 3</h4>' +
+            '<p>Lorem Ipsum</p>' +
+          '</div>' +
+          '<div class="tabbody">' +
+            '<h4>Tabbody Heading 4</h4>' +
+            '<p>Lorem Ipsum</p>' +
+          '</div>' +
+        '</div>');
+    });
+
+
+    describe('NeoTabs instance', function () {
+
+      var $instance = new NeoTabs($fixture);
+
+      it('tests if constructor returns a NeoTabs instance', function () {
+        expect(typeof($instance)).toBe('object');
+        expect($instance.constructor).toBe(NeoTabs);
+      });
     });
   });
 });
