342c3af9abd7d3979e7ea6b09d1b7a668227f0bb
Pascal Precht <pascal.precht@googlemail.com>
2012-11-07 15:43:59 +0100

    Updated specs and jshint task in gruntjs flow

--- tests/unit/specs.js
+++ tests/unit/specs.js
@@ -1,64 +1,75 @@
-'use strict';
+(function () {
 
-describe('jQuery NeoTabs TestSuite', function () {
+  "use strict";
 
-  var fixture = '<div class="tabs">' +
-    '<div class="tabbody">' +
-      '<h4>Tabbody Heading</h4>' +
-      '<p>Lorem Ipsum</p>' +
-    '</div>' +
-    '<div class="tabbody">' +
-      '<h4>Tabbody Heading 2</h4>' +
-      '<p>Lorem Ipsum</p>' +
-    '</div>' +
-    '<div class="tabbody">' +
-      '<h4>Tabbody Heading 3</h4>' +
-      '<p>Lorem Ipsum</p>' +
-    '</div>' +
-    '<div class="tabbody">' +
-      '<h4>Tabbody Heading 4</h4>' +
-      '<p>Lorem Ipsum</p>' +
-    '</div>' +
-  '</div>';
+  describe('jQuery NeoTabs TestSuite', function () {
 
-  describe('Setup', function () {
+    var fixture = '<div class="tabs">' +
+      '<div class="tabbody">' +
+        '<h4>Tabbody Heading</h4>' +
+        '<p>Lorem Ipsum</p>' +
+      '</div>' +
+      '<div class="tabbody">' +
+        '<h4>Tabbody Heading 2</h4>' +
+        '<p>Lorem Ipsum</p>' +
+      '</div>' +
+      '<div class="tabbody">' +
+        '<h4>Tabbody Heading 3</h4>' +
+        '<p>Lorem Ipsum</p>' +
+      '</div>' +
+      '<div class="tabbody">' +
+        '<h4>Tabbody Heading 4</h4>' +
+        '<p>Lorem Ipsum</p>' +
+      '</div>' +
+    '</div>';
 
-    it('tests if jQuery is loaded', function () {
-      expect(!!window.$).toBe(true);
-      expect(typeof(window.$)).toBe('function');
+    describe('Setup', function () {
+
+      it('tests if jQuery is loaded', function () {
+        expect(!!window.$).toBe(true);
+        expect(typeof(window.$)).toBe('function');
+      });
+
+      it('tests if NeoTabs is a jQuery plugin', function () {
+        expect(typeof($.fn.neoTabs)).toBe('function');
+      });
+
+      it('tests if NeoTabs constructor is avaible in global scope', function () {
+        expect(!!window.NeoTabs).toBe(true);
+        expect(typeof(window.NeoTabs)).toBe('function');
+      });
     });
 
-    it('tests if NeoTabs is a jQuery plugin', function () {
-      expect(typeof($.fn.neoTabs)).toBe('function');
+    describe('Initialization', function () {
+      var $el = $(fixture),
+          $neoTabs = new NeoTabs($el);
+
+      it('should assign an element', function () {
+        expect($neoTabs.$el).toBe($el);
+      });
+
+      it('should inject a tabs list', function () {
+        expect($neoTabs.$el).toContain('.tabs-list');
+      });
+
+      it('should prepend the tabs list by default', function () {
+        expect($neoTabs.$el.children(0)).toBe('ul.tabs-list');
+      });
     });
 
-    it('tests if NeoTabs constructor is avaible in global scope', function () {
-      expect(!!window.NeoTabs).toBe(true);
-      expect(typeof(window.NeoTabs)).toBe('function');
+    describe('Public API', function () {
+      var $el = $(fixture),
+          $neoTabs = new NeoTabs($el);
+
+      it('should have a method to activate tabs', function () {
+        var methodExists = (typeof($neoTabs.activateTab) === 'function');
+        expect(methodExists).toBe(true);
+      });
+
+      it('should have a method to open a dropdown', function () {
+        var methodExists = (typeof($neoTabs.openDropdown) === 'function');
+        expect(methodExists).toBe(true);
+      });
     });
   });
-
-  describe('Initialization', function () {
-    var $el = $(fixture),
-        $neoTabs = new NeoTabs($el);
-
-    it('should assign an element', function () {
-      expect($neotabs.$el).toBe($el);
-    });
-  });
-
-  describe('Public API', function () {
-    var $el = $(fixture),
-        $neoTabs = new NeoTabs($el);
-
-    it('should have a method to activate tabs', function () {
-      var methodExists = (typeof($neoTabs.activateTab) === 'function')
-      expect(methodExists).toBe(true);
-    });
-
-    it('should have a method to open a dropdown', function () {
-      var methodExists = (typeof($neoTabs.openDropdown) == 'function');
-      expected(methodExists).toBe(true);
-    });
-  });
-});
+}());
